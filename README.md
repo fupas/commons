b99da9052a41ef46ed356bfe7d99c9322e17b813
Michael Kuehl <hello@txs.vc>
2021-03-02 16:20:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# fupas/commons
+
+A repo with shared, common packages used in most of the projects by txs.vc, podops.dev etc.
+
+This is very opinionated.
+
+## Modules
+
+* env
+* util
+* sanitize
---
+++ pkg/env/env.go
@@ -0,0 +1,29 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+// GetString returns the environment variable ENV['env'] or def if 'env' is not set.
+// Note: def is only returned if the 'env' is not set, i.e. an EMPTY 'env' is still returned !
+func GetString(env, def string) string {
+	e, ok := os.LookupEnv(env)
+	if ok {
+		return e
+	}
+	return def
+}
+
+// GetInt returns the environment variable ENV['env'] or def if 'env' is not set.
+func GetInt(env string, def int64) int64 {
+	e, ok := os.LookupEnv(env)
+	if ok {
+		v, err := strconv.ParseInt(e, 10, 64)
+		if err != nil {
+			return def
+		}
+		return v
+	}
+	return def
+}
